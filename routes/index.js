fd187e9ec253480715d3bfed6b5e32a25be902cc
dnahotiori <dnahotiori@163.com>
2018-10-11 23:11:23 +0800

    2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var path = require("path");
 //var mallSdkUtilitys = require("../MallSDK/SDKUtiltiys");
 var mallsdkAPi = require("../MallSDK/RequestAPI");
 var DefaultClient = require("../MallSDK/DefaultClient");
@@ -36,7 +38,23 @@ router.get('/', function (req, res, next) {
     }).catch(err => {
         console.log(err);
     });
+    res.redirect("/Home/dowloand")
     res.render("index", { title: "WelCome" });
+});
+
+router.get("/dowloand", function (req, res, next) {
+    //let referer= req.headers.referer;
+    console.log(req);
+    res.send(req.header);
+    // res.end();
+    // var filePath = path.join(__dirname, '../AliPayKey/2017080208002578_alipay_public_key.pem');
+    // var stats = fs.statSync(filePath);
+    // res.set({
+    //     'Content-Type': 'application/octet-stream', //告诉浏览器这是一个二进制文件
+    //     'Content-Disposition': 'attachment; filename=2017080208002578_alipay_public_key.pem', //告诉浏览器这是一个需要下载的文件
+    //     'Content-Length': stats.size  //文件大小
+    // });
+    // fs.createReadStream(filePath).pipe(res);
 });
 
 module.exports = router;
